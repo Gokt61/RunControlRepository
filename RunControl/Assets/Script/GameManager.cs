4bc04dc6ef3af393a003d38edd2db6cc30bd3f2f
Gokt61 <49290553+Gokt61@users.noreply.github.com>
2023-03-27 15:19:56 +0300

    Object Pooling
    başlangıç olarak Object Pooling kodunu ekledim

--- RunControl/Assets/Script/GameManager.cs
+++ RunControl/Assets/Script/GameManager.cs
@@ -8,6 +8,10 @@ public class GameManager : MonoBehaviour
     public GameObject PrefabKarakter;
     public GameObject DogmaNoktasi;
     public GameObject VarisNoktasi;
+
+
+    public List<GameObject> Karakterler;
+
     void Start()
     {
         
@@ -17,7 +21,15 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            Instantiate(PrefabKarakter, DogmaNoktasi.transform.position, Quaternion.identity);
+            foreach (var item in Karakterler)
+            {
+                if (!item.activeInHierarchy)
+                {
+                    item.transform.position = DogmaNoktasi.transform.position;
+                    item.SetActive(true);
+                    break;
+                }
+            }
         }
     }
 }
